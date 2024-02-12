bd666ff8aefb0a14cf63a4599c570c47b4fcc0bc
희제 <89143804+HeeJeYang@users.noreply.github.com>
2024-02-12 23:54:55 +0900

    Title: 서로 다른 숫자, Time: 368ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 240212/서로 다른 숫자/distinct-numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> numbers = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
+
+        System.out.println(numbers.size());
+    }
+}
